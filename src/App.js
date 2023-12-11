476b0ce6e055780ff3ae7f78bad2549a498cf85d
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-12-11 10:52:52 +0200

    Done about,statistics,brands,footer

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div>
-HELLO WORLD
-    </div>
+      <div>HELLO WORLD</div>
     </div>
   );
 }
---
+++ src/Components/Statistics.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "../Styles/statistics.css";
+import { useEffect } from "react";
+
+const Statistics = () => {
+  function startCountAnimation(targetId, targetValue, duration) {
+    const targetElement = document.getElementById(targetId);
+    const increment = targetValue / (duration * 1000);
+
+    let currentValue = 0;
+
+    const intervalId = setInterval(function () {
+      if (currentValue < targetValue) {
+        currentValue += increment;
+        targetElement.textContent = Math.round(currentValue);
+      } else {
+        targetElement.textContent = targetValue;
+        clearInterval(intervalId);
+      }
+    }, 1);
+  }
+
+  useEffect(() => {
+    startCountAnimation("clientCount", 75, 1);
+    startCountAnimation("orderCount", 95, 1);
+    startCountAnimation("areaCount", 150, 1);
+  }, []);
+  return (
+    <div className="statistics">
+      <div className="stClient">
+        <span id="clientCount">0</span>
+        <br />
+        Happy customers
+      </div>
+      <div className="stOrders">
+        <span id="orderCount">0</span>
+        <br />
+        Orders Done
+      </div>
+      <div className="stArea">
+        <span id="areaCount">0</span>
+        <br />
+        Areas reached
+      </div>
+    </div>
+  );
+};
+
+export default Statistics;
